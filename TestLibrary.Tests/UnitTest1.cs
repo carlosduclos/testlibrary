d9f2818f013f02263ee32f6c4e41b0d5f7cbffc7
Carlos Manuel Duclos Vergara <carlos.duclos@software-innovation..com>
2015-05-18 15:37:36 +0200

    Adding test library project

---
+++ TestLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TestLibrary;
+namespace TestLibrary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 object1 = new Class1();
+            object1.DoSomething(10);
+        }
+    }
+}
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public void DoSomething(int value)
+        {
+            int someOtherValue = value + 10;
+        }
+    }
+}
